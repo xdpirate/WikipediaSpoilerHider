217c46f044b00333c250d3496d0823277beeecfb
Lars-Christian Bakken <retrocrew.djlarz@gmail.com>
2013-05-13 04:22:56 +0200

    debump version and update xpi

--- extension/content/wikipediaspoilerhider.js
+++ extension/content/wikipediaspoilerhider.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @id             dc09f455-deee-4eec-9660-9da7394b1b71
 // @name           Wikipedia Spoiler Hider
-// @version        1.2.4
+// @version        1.2.3
 // @author         xpdite
 // @description    Hides potential spoilers on Wikipedia
 // @include        /^https?://.*\.wikipedia\.org/wiki/.*$/
